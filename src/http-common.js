3514d0dfe5c82e1f441b5642f2439d28ea5788b9
gabriel <gabrielalmeidads@gmail.com>
2020-09-04 19:27:34 -0300

    resolvendo problemas no deploy

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
+// http://localhost:8081/
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: 'https://api-igti-modulo-04.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
